5307faec919b7fd6526b686ffa59c7f7b6cf91fa
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-04-28 19:55:01 +0600

    genius car mongodb connected

---
+++ src/Pages/ManageServices/ManageService.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import useServices from '../../hooks/useServices';
+
+const ManageService = () => {
+    const [services, setServices] = useServices()
+    const handleDelete = _id => {
+        const proceed = window.confirm('Are you sure?')
+        if (proceed) {
+            const url = `http://localhost:5000/service/${_id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .them(data => {
+                    const remaining = services.filter(service => service._id !== _id)
+                    setServices(remaining)
+                })
+        }
+    }
+    return (
+        <div>
+            <h2>Manage your services</h2>
+            {
+                services.map(service => <div className='d-flex m-4' key={service._id}><h5>{service.name}</h5>
+                    <button onClick={() => handleDelete(service._id)} className='btn btn-primary ms-2'>Delete</button>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageService;
